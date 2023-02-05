fba1d53927cd6c14f1a7f01f2c3d8c8acdfe0ac0
Saeedur Rahman <saeedur1@outlook.com>
2023-02-05 22:33:01 +0000

    version 1.0

--- source/js/charsets.js
+++ source/js/charsets.js
@@ -3,4 +3,5 @@ export default [
     {name: 'Uppercase', charset: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'},
     {name: 'Lowercase', charset: 'abcdefghijklmnopqrstuvwxyz'},
     {name: 'Numbers', charset: '0123456789'},
+    {name: 'Special Characters', charset: '!@#$%&*()+-;:,.?'},
 ];
