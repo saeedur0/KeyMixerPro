aee9479e0a0c8eb1305cef43c059af75fee856ac
Saeedur Rahman <saeedur1@outlook.com>
2023-02-05 02:24:14 +0000

    almost done, need to edit css

--- source/js/charsets.js
+++ source/js/charsets.js
@@ -1,6 +1,6 @@
 // Character sets for password generation
 export default [
-    {name: 'Uppercase (A-Z)', charset: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'},
-    {name: 'Lowercase (a-z)', charset: 'abcdefghijklmnopqrstuvwxyz'},
-    {name: 'Numbers (0-9)', charset: '0123456789'},
+    {name: 'Uppercase', charset: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'},
+    {name: 'Lowercase', charset: 'abcdefghijklmnopqrstuvwxyz'},
+    {name: 'Numbers', charset: '0123456789'},
 ];
